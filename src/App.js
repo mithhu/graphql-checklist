8ee80d58a7847f1d6aa73d23ca86c52bf11fb129
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2020-06-07 15:19:00 +0600

    used Apollo cache to update UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
@@ -24,13 +24,64 @@ const TOGGLE_TODO = gql`
   }
 `;
 
+const ADD_TODO = gql`
+  mutation addTodo($text: String!) {
+    insert_todos(objects: { text: $text }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: uuid!) {
+    delete_todos(where: { id: { _eq: $id } }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
 function App() {
+  const [todoText, setTodoText] = useState("");
   const { data, loading, error } = useQuery(GET_TODOS);
   const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setTodoText(""),
+  });
+  const [deleteTodo] = useMutation(DELETE_TODO);
 
   async function handleToggleTodo({ id, done }) {
-    const data = await toggleTodo({ variables: { id, done: !done } });
-    console.log(data);
+    await toggleTodo({ variables: { id, done: !done } });
+  }
+
+  async function handleAddTodo(event) {
+    event.preventDefault();
+    if (!todoText) return;
+    await addTodo({
+      variables: { text: todoText },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
+  }
+
+  async function handleDeleteTodo({ id }) {
+    const isConfirmed = window.confirm("Do you want to delete this todo?");
+    if (isConfirmed) {
+      await deleteTodo({
+        variables: { id },
+        update: (cache) => {
+          const prevData = cache.readQuery({ query: GET_TODOS });
+          const newTodos = prevData.todos.filter((todo) => todo.id !== id);
+          cache.writeQuery({ query: GET_TODOS, data: { todos: newTodos } });
+        },
+      });
+    }
   }
   if (loading) return <div>loading todos......</div>;
   if (error) return <div>Error loading todos......</div>;
@@ -44,11 +95,13 @@ function App() {
           ✅
         </span>
       </h1>
-      <form className="mb3">
+      <form onSubmit={handleAddTodo} className="mb3">
         <input
           className="pa2 f4 b--dashed"
           type="text"
           placeholder="Write your code"
+          onChange={(event) => setTodoText(event.target.value)}
+          value={todoText}
         />
         <button className="pa2 f4 bg-green" type="submit">
           Create
@@ -60,7 +113,10 @@ function App() {
             <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>
               {todo.text}
             </span>
-            <button className="bg-transparent bn f4">
+            <button
+              onClick={() => handleDeleteTodo(todo)}
+              className="bg-transparent bn f4"
+            >
               <span className="red">&times;</span>
             </button>
           </p>
